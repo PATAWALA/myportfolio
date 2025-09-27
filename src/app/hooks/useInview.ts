09e2316a94b70236b0890fbec72b433f025b846f
Abdoulaye PATAWALA <patawalaabdoulaye2003@gmail.com>
2025-09-27 12:43:58 +0100

    Mis a jour de UseInview.ts

--- src/app/hooks/useInview.ts
+++ src/app/hooks/useInview.ts
@@ -1,19 +1,25 @@
 "use client"
 import { useState, useEffect, useRef } from "react"
 
-export function useInView(threshold = 0.1) {
+export function useInView(threshold = 0.1, triggerImmediately = true) {
   const ref = useRef<HTMLDivElement>(null)
-  const [isInView, setIsInView] = useState(false)
+  const [isInView, setIsInView] = useState(triggerImmediately) // true dès le départ si triggerImmediately = true
 
   useEffect(() => {
     if (!ref.current) return
+    // Si on veut déclencher immédiatement, pas besoin d'observer
+    if (triggerImmediately) {
+      setIsInView(true)
+      return
+    }
+
     const observer = new IntersectionObserver(
       ([entry]) => setIsInView(entry.isIntersecting),
       { threshold }
     )
     observer.observe(ref.current)
     return () => observer.disconnect()
-  }, [threshold])
+  }, [threshold, triggerImmediately])
 
   return { ref, isInView }
 }
